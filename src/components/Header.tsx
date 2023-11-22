90cdf41f8d58242ab54b891d1d09d7b4258c16e2
yaman jain <jyaman694@gmail.com>
2023-11-22 12:07:26 +0530

    logo decided working on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,37 @@
+import search from '../assets/svg/search.svg'
+import logo from '../assets/svg/qnilogo.svg'
+import contact from '../assets/svg/contact.svg'
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    // <div>
+    <header className="header__container container">
+      <div className="logo">
+        <img src={logo} alt="Qni logo" />
+        <span className="companyName">Quality & improvisation</span>
+      </div>
+      <nav className="header__nav">
+        <ul className="nav__links">
+          <li className="nav__link">Features</li>
+          <li className="nav__link">Industries</li>
+          <li className="nav__link">Pricing</li>
+          <li className="nav__link">Customers</li>
+          <li className="nav__link">About</li>
+
+          <li className="nav__link contact">
+            <div>
+              <img src={contact} alt="" />
+              <span>Contact</span>
+            </div>
+          </li>
+          <li className="nav__link search">
+            <img src={search} alt="" />
+          </li>
+          <li className="nav__link">
+            <button className="btn nav__button">Book a demo</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    // </div>
   )
 }
