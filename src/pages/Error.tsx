69b1bd75561d069273f11ebd3c80c9ed8d12b073
Ayush kumar <kumarayushkumar@protonmail.com>
2023-12-05 16:47:20 +0530

    Remove unused import in Error.tsx

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import NotFound from '../components/NotFound'
 
 export default function Error() {
   const navigate = useNavigate()
-  const location = useLocation()
 
   useEffect(() => {
     const redirectTimeout = setTimeout(() => {
