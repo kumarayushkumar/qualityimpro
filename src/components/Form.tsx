2e5b6591f7b886cb9fc5fcd14707dd597c2cb612
yaman jain <jyaman694@gmail.com>
2023-12-08 20:27:43 +0530

    added retool webhook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -44,7 +44,30 @@ export default function Form() {
   const onSubmit = (data: IForm) => {
     data.created_time = Timestamp.now()
     saveData(data)
-      .then(() => {
+      .then(async() => {
+        const response = await fetch(
+          `https://api.retool.com/v1/workflows/8a84bd95-cc37-4d64-92ba-3677afd7c1d1/startTrigger?workflowApiKey=${
+            import.meta.env.VITE_RETOOL_API
+          }`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              firstname: data.firstname,
+              lastname: data.lastname,
+              email: data.email,
+              company: data.company,
+              phone: data.phone,
+              category: data.category
+            })
+          }
+        )
+
+        const json = await response.json();
+        console.log(json);
+
         reset()
         const formBtn = document.querySelector('#form-btn') as HTMLButtonElement
         const formContainer = document.querySelector('.form__container') as HTMLDivElement
